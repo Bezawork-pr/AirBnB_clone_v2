6cda7246e758674da22fb8bebdbc9623407b2e0d
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-08 23:37:05 -0800

    Add identification with password

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 -- This will be used in production of the airbnb clone project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES on hbnb_dev_db.* TO hbnb_dev@localhost;
-GRANT SELECT ON performance_schema .* TO hbnb_dev@localhost;
+GRANT ALL PRIVILEGES on hbnb_dev_db.* TO hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema .* TO hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
