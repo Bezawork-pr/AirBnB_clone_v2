c8bee863c8db6daa8f4afbc900bc88cefeff5bbd
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-08 23:53:50 -0800

    Debug only checker fail

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,5 @@
--- SQL script for setting up the mysql development server
--- This will be used in production of the airbnb clone project
+-- a script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES on hbnb_dev_db.* TO hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema .* TO hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY hbnb_dev_pwd;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
