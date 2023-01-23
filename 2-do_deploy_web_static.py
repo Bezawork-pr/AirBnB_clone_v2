9e74dd06ca9bf93d2fba127f9dee77dfe36bc244
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-23 03:50:54 -0800

    Get file name first before spliting file extension

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@ that distributes an archive to your web servers,
 using the function do_deploy:"""
 from datetime import datetime
 from fabric.api import *
+from os import path
 
 
 def do_pack():
@@ -20,13 +21,16 @@ def do_pack():
 
 def do_deploy(archive_path):
 	"""distributes an archive to your web servers"""
+        if path.exists(archive_path) is False:
+            return False
 	try:
-		no_ext, ext = archive_path.split(".")
+                file_name = archive_path.split("/")
+		no_ext, ext = file_name[1].split(".")
 		put(archive_path, "/tmp ")
 		run("mkdir -p /data/web_static/releases/{}".format(no_ext))
-		run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(archive_path, no_ext))
-		run("rm /tmp/{}".format(archive_path))
-		run("mv /data/web_static/releases/{}/web_static/* data/web_static/releases/{}".format)
+		run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file_name, no_ext))
+		run("rm /tmp/{}".format(file_name))
+		run("mv /data/web_static/releases/{}/web_static/* data/web_static/releases/{}".format(no_ext, no_ext))
 		run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext))
 		run("rm -rf /data/web_static/current")
 		run("ln -s /data/web_static/releases/{} /data/web_static/current".format(no_ext))
