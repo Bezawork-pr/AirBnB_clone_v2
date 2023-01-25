896c5c4637179056a1e03f7681f5d2ce1426d7f4
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-25 04:03:38 -0800

    Add servers to env.hosts

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""a Fabric script (based on the file 
-1-pack_web_static.py) that distributes an 
-archive to your web servers, 
+"""a Fabric script (based on the file
+1-pack_web_static.py) that distributes an
+archive to your web servers,
 using the function do_deploy:"""
 from os.path import exists
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
+env.hosts = ['34.202.158.152', '100.25.34.22']
 
 
 def do_deploy(archive_path):
