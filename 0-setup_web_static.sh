129559573c119bbc18ad3cd4689ef7e710b12753
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-25 00:36:17 -0800

    Add single quotes for sed instead of double

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "38i \\\tlocation /htbn_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default 
+sudo sed -i '38i \\\tlocation /htbn_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default 
 sudo service nginx restart
