86335c3f7af55ee6ea2f87de6bb27eace9af7b1f
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-25 04:10:17 -0800

    Revert back to old code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
-"""a Fabric script (based on the file
-1-pack_web_static.py) that distributes an
-archive to your web servers,
+"""Write a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
 using the function do_deploy:"""
-from os.path import exists
-from fabric.api import env
-from fabric.api import put
+from datetime import datetime
 from fabric.api import run
+from fabric.api import put
+from fabric.api import env
+import os.path
+
 env.hosts = ['34.202.158.152', '100.25.34.22']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers,
-    using the function do_deploy"""
-    if exists(archive_path) is False:
+    """distributes an archive to your web servers"""
+    if os.path.exists(archive_path) is False:
         return False
-    f_we = archive_path.split("/")[-1]
-    f_wo = f_we.split(".")[0]
-    path = "/data/web_static/releases"
-    path2 = "/data/web_static/current"
     try:
-        put(archive_path, "/tmp/{}").format(f_we)
-        run("rm -rf {}/{}/").format(path, f_wo)
-        run("mkdir -p {}/{}/").format(path, f_wo)
-        run("tar -xzf /tmp/{} -C {}/{}/").format(path, f_we, f_wo)
-        run("rm /tmp/{}").format(f_we)
-        run("mv {0}/{1}/web_static/* {0}/{1}/").format(path, f_wo)
-        run("rm -rf {}/{}/web_static").format(path, f_wo)
-        run("rm -rf {}").format(path2)
-        run("ln -s {}/{}/ {}").format(path, f_wo, path2)
+        with_ext = archive_path.split("/")[-1]
+        no_ext = with_ext.split(".")[0]
+        put(archive_path, "/tmp/{}".format(with_ext))
+        run("sudo rm -rf /data/web_static/releases/{}".format(no_ext))
+        run("sudo mkdir -p /data/web_static/releases/{}".format(no_ext))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}".
+            format(with_ext, no_ext))
+        run("sudo rm /tmp/{}".format(with_ext))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+             /data/web_static/releases/{}".format(no_ext, no_ext))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".
+            format(no_ext))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{} \
+                /data/web_static/current".format(no_ext))
+        print("New version deployed!")
         return True
     except Exception as NotDeployed:
         return False
