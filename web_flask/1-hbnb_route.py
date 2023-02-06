1d15181cc1bda733d93c9f74b7bf250ac797fa42
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-06 00:58:31 -0800

    Fix the routing

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,7 +16,7 @@ def hello():
     return 'Hello HBNB!'
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """A function that return HBNB"""
     return 'HBNB'
