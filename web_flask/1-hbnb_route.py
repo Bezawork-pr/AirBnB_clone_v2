e88623fc4886a5f07428b60cb1161cf865675a13
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-06 00:57:25 -0800

    Add second function that returns HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+    Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """A function that return Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """A function that return HBNB"""
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
