3c8296800ab18706a5959d1cd79ba4fd8b91f3b0
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-06 22:45:42 -0800

    Return only City objects from storage linked to current state

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String
@@ -18,8 +19,9 @@ class State(BaseModel, Base):
         def cities(self):
             """return the list of City"""
             cities = []
-            for ids, value in models.storage.all().items():
-                cls, ids = ids.split(".")
-                if cls.__name__ == "City":
-                    cities.append(value)
+            for _ids, city in models.storage.all().items():
+                cls, _id = _ids.split(".")
+                if cls == "City":
+                    if city.state_id == self.id:
+                        cities.append(city)
             return cities
